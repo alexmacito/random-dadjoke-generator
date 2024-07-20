0b1a4a2d2c63232cbfd1e1080c347a2971c5655c
Alex Mac <144024631+alexmacito@users.noreply.github.com>
2024-07-20 01:48:47 +0100

    Correcting speech function JS and comments added + edited CSS

--- script.js
+++ script.js
@@ -17,19 +17,21 @@ const requestOptions = {
 function randomJoke(){
     jokeBtn.classList.add("loading");
     jokeBtn.innerText = "Loading Joke...";
+    // fetching random jokes/data from the API and parsing it into JavaScript object 
     fetch("https://icanhazdadjoke.com/", requestOptions)
   .then((response) => response.json())
   .catch((error) => console.error(error)).then(result => {
         jokeText.innerText = result.joke;
         jokeBtn.classList.remove("loading");
-        jokeBtn.innerText = "Give me an other one";
+        jokeBtn.innerText = "Give me another one";
     });
 }
 
 speechBtn.addEventListener("click", ()=>{
     if(!jokeBtn.classList.contains("loading")){
-        let utterance = new SpeechSynthesisUtterance(`jokeText.innerText`);
-        synth.speak(utterance);
+        // the SpeechSynthesisUtterance is a web speech API tha represents a speech request
+        let utterance = new SpeechSynthesisUtterance(`${jokeText.innerText}`);
+        synth.speak(utterance); // speak method of SpeechSynthesis speaks the utterance
         setInterval(()=>{
             !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
         }, 10);
@@ -37,12 +39,14 @@ speechBtn.addEventListener("click", ()=>{
 });
 
 copyBtn.addEventListener("click", ()=>{
+    // copying the joke text on copyBtn click
+    // writeText() property writes the specified text string to the system clipboard
     navigator.clipboard.writeText(jokeText.innerText);
 });
 
 twitterBtn.addEventListener("click", ()=>{
     let tweetUrl = `https://twitter.com/intent/tweet?url=${jokeText.innerText}`;
-    window.open(tweetUrl, "_blank");
+    window.open(tweetUrl, "_blank"); //opening a new twitter tab with passing joke in the url
 });
 
 jokeBtn.addEventListener("click", randomJoke);
